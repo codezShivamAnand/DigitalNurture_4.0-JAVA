9444421621a1a1bf0c55f21c2496575ddb1a768e
Shivam Anand <proff.shivam17anand@gmail.com>
2025-06-23 08:00:07 +0530

    modified implementation

--- Week1/Module1/SingletonPatternExample/Logger.java
+++ Week1/Module1/SingletonPatternExample/Logger.java
@@ -1,13 +1,10 @@
 public class Logger {
-    // Private static instance of Logger (eager initialization or lazy init can be used)
     private static Logger instance;
 
-    // Private constructor to prevent instantiation from other classes
     private Logger() {
         System.out.println("Logger instance created");
     }
 
-    // Public static method to provide access to the single instance
     public static Logger getInstance() {
         if (instance == null) {
             instance = new Logger();  // lazy initialization
@@ -15,7 +12,6 @@ public class Logger {
         return instance;
     }
 
-    // Example method to demonstrate usage
     public void log(String message) {
         System.out.println("Log: " + message);
     }
