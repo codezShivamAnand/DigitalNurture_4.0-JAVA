4db5e385bc037a872edd56a3385ef66be72dd7e3
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-29 00:39:59 +0530

    Removed duplicate folder SpringDataJPAwithHibernate

--- Week3/Spring Data JPA with Hibernate/Exercise 4/src/main/java/com/cognizant/difference/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.difference.repository;
-
-import com.cognizant.difference.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
-
